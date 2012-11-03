90c3fd5ede62cec06da8a237537dfc04217136ea
Diogo Mónica <diogo.monica@gmail.com>
2012-11-02 21:58:23 -0700

    Most of the functionality implemented

--- gocert.go
+++ gocert.go
@@ -7,43 +7,85 @@ import (
 	"time"
 	"fmt"
 	"crypto/x509"
-	//"encoding/pem"
+	"crypto/x509/pkix"
+	"math"
+	"os"
+	"flag"
+	"encoding/pem"
+	"io/ioutil"
+
 )
 
-func handle_cert(cert *x509.Certificate) {
-	// block := pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}
-	//pemdata := string(pem.EncodeToMemory(&block))
-	fmt.Println(cert.Subject.CommonName)
-	fmt.Println(cert.Version)
-	fmt.Println(cert.SerialNumber)
-	fmt.Println(cert.Subject)
-	fmt.Println(cert.NotBefore, cert.NotAfter)
+func printPkix(pkixName pkix.Name) {
+	fmt.Printf("%s - %s\n", pkixName.CommonName, pkixName.Organization)
+}
 
-	//fmt.Println(pemdata)
+func print_cert(cert *x509.Certificate) {
+	fmt.Printf("CommonName: ")
+	printPkix(cert.Subject)
+	fmt.Printf("Issuer: ")
+	printPkix(cert.Issuer)
+	fmt.Printf("Alt Names: %s\n", cert.DNSNames)
+	timeDifference := cert.NotAfter.Sub(time.Now())
+	fmt.Printf("Expires in: %v days\n", math.Floor(timeDifference.Hours()/24))
+	fmt.Printf("Version: %v ; Serial: %s\n", cert.Version, cert.SerialNumber)
+	fmt.Printf("BasicConstraintsValid: %v ; IsCA: %v ; MaxPathLen: %v\n", 
+				cert.BasicConstraintsValid, cert.IsCA, cert.MaxPathLen)
+}
+
+func save_cert(cert *x509.Certificate) {
+        block := pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}
+        pemdata := string(pem.EncodeToMemory(&block))
+
+        err := ioutil.WriteFile(cert.Subject.CommonName + ".pem", []byte(pemdata), 0644)
+	    if err != nil { panic(err) }
+}
+
+func usage() {
+    fmt.Fprintf(os.Stderr, "usage: %s [-s] [-c] hostname:port\n", os.Args[0])
+    flag.PrintDefaults()
+    os.Exit(2)
 }
 
 func main() {
 
-	config := tls.Config{InsecureSkipVerify: true}
+	var chain = flag.Bool("chain", false, "Print details about all the certificates on the chain")
+	var save = flag.Bool("save", false, "Save the PEM to the local disk in the format of #{CN}.pem")
+	flag.Parse()
 
-	conn, err := net.DialTimeout("tcp", "squareup.com:443", 2*time.Second)
+    args := flag.Args()
+    if len(args) < 1 {
+    	usage()
+    }
+
+	config := tls.Config{InsecureSkipVerify: true}
+	conn, err := net.DialTimeout("tcp", args[0], 2*time.Second)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	client := tls.Client(conn, &config)
-
 	err = client.Handshake()
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	state := client.ConnectionState()
+	
+	certificates := []*x509.Certificate{}
+	if *chain {
+		certificates = state.PeerCertificates
+	} else {
+		certificates = []*x509.Certificate{state.PeerCertificates[0]}
+	}
 
-	for _, cert := range state.PeerCertificates {
-		handle_cert(cert)
+	for _, cert := range certificates {
+		print_cert(cert)
+		if *save {
+			save_cert(cert)
+		}
+		fmt.Println("--------------------")
 	}
 	client.Close()
 }
