41a3627b076755a838a041e3af39c871cdc0bcbc
Diogo Mónica <diogo.monica@gmail.com>
2012-11-02 22:20:28 -0700

    Added instructions to the README

--- README.md
+++ README.md
@@ -8,3 +8,67 @@ A small go program to print details on X509 certificates and safe them in .pem f
 - Because I can never remember openssl's syntax. The s_client -connect is not intuitive. 
 - Because I want a better way for displaying certificates, with emphasis on what matters.
 - Because I want to be able to extract remote certificates .pem's easily
+
+# Installation 
+
+- Just build gocert.go and move it somewhere in your path. For example, if you are running OSX (this you already have the GO environment installed):
+
+	`cd /tmp; curl -O https://raw.github.com/diogomonica/gocert/master/gocert.go;
+	go build gocert.go; mv gocert /usr/local/bin`
+
+# Usage
+
+	usage: gocert [-s] [-c] hostname:port
+	  -chain=false: Print details about all the certificates on the chain
+	  -save=false: Save the PEM to the local disk in the format of #{CN}.pem
+
+# Examples
+
+### Retrieving information about the certificate of diogomonica.com:
+
+	# gocert diogomonica.com:443	
+	CommonName: www.diogomonica.com - []
+	Issuer: StartCom Class 1 Primary Intermediate Server CA - [StartCom Ltd.]
+	Alt Names: [www.diogomonica.com diogomonica.com]
+	Expires in: 359 days
+	Version: 3 ; Serial: 516289
+	BasicConstraintsValid: true ; IsCA: false ; MaxPathLen: -1
+	--------------------
+
+### Retrieving information about the full chain of diogomonica.com:
+
+	# gocert -chain diogomonica.com:443
+	CommonName: www.diogomonica.com - []
+	Issuer: StartCom Class 1 Primary Intermediate Server CA - [StartCom Ltd.]
+	Alt Names: [www.diogomonica.com diogomonica.com]
+	Expires in: 359 days
+	Version: 3 ; Serial: 516289
+	BasicConstraintsValid: true ; IsCA: false ; MaxPathLen: -1
+	--------------------
+	CommonName: StartCom Class 1 Primary Intermediate Server CA - [StartCom Ltd.]
+	Issuer: StartCom Certification Authority - [StartCom Ltd.]
+	Alt Names: []
+	Expires in: 1816 days
+	Version: 3 ; Serial: 24
+	BasicConstraintsValid: true ; IsCA: true ; MaxPathLen: -1
+	--------------------
+	CommonName: StartCom Certification Authority - [StartCom Ltd.]
+	Issuer: StartCom Certification Authority - [StartCom Ltd.]
+	Alt Names: []
+	Expires in: 8719 days
+	Version: 3 ; Serial: 1
+	BasicConstraintsValid: true ; IsCA: true ; MaxPathLen: -1
+	--------------------
+
+### Retrieving and saving in PEM format the certificate of diogomonica.com:
+
+	# gocert -save diogomonica.com:443
+	CommonName: www.diogomonica.com - []
+	Issuer: StartCom Class 1 Primary Intermediate Server CA - [StartCom Ltd.]
+	Alt Names: [www.diogomonica.com diogomonica.com]
+	Expires in: 359 days
+	Version: 3 ; Serial: 516289
+	BasicConstraintsValid: true ; IsCA: false ; MaxPathLen: -1
+	--------------------
+	# * ls -alh www.diogomonica.com.pem 
+	-rw-r--r--  1 diogo  diogo   2.8K Nov  2 22:19 www.diogomonica.com.pem
--- gocert.go
+++ gocert.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-	"crypto/tls"
-	"net"
-	"log"
-	"time"
-	"fmt"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"math"
-	"os"
-	"flag"
-	"encoding/pem"
-	"io/ioutil"
+    "crypto/tls"
+    "net"
+    "log"
+    "time"
+    "fmt"
+    "crypto/x509"
+    "crypto/x509/pkix"
+    "math"
+    "os"
+    "flag"
+    "encoding/pem"
+    "io/ioutil"
 
 )
 
 func Bold(str string) string {
-    return "\033[1m" + str + "\033[0m"
+      return "\033[1m" + str + "\033[0m"
 }
 
 func BoldPrintf(str string) {
-	fmt.Printf(Bold(str))
+    fmt.Printf(Bold(str))
 }
 
 func printPkix(pkixName pkix.Name) {
-	fmt.Printf("%s - %s\n", pkixName.CommonName, pkixName.Organization)
+    fmt.Printf("%s - %s\n", pkixName.CommonName, pkixName.Organization)
 }
 
 func print_cert(cert *x509.Certificate) {
-	BoldPrintf(Bold("CommonName: "))
-	printPkix(cert.Subject)
-	BoldPrintf("Issuer: ")
-	printPkix(cert.Issuer)
-	BoldPrintf("Alt Names: ")
-	fmt.Printf("%s\n", cert.DNSNames)
-	timeDifference := cert.NotAfter.Sub(time.Now())
-	BoldPrintf("Expires in: ")
-	fmt.Printf("%v days\n", math.Floor(timeDifference.Hours()/24))
-	fmt.Printf("Version: %v ; Serial: %s\n", cert.Version, cert.SerialNumber)
-	fmt.Printf("BasicConstraintsValid: %v ; IsCA: %v ; MaxPathLen: %v\n", 
-				cert.BasicConstraintsValid, cert.IsCA, cert.MaxPathLen)
+    BoldPrintf(Bold("CommonName: "))
+    printPkix(cert.Subject)
+    BoldPrintf("Issuer: ")
+    printPkix(cert.Issuer)
+    BoldPrintf("Alt Names: ")
+    fmt.Printf("%s\n", cert.DNSNames)
+    timeDifference := cert.NotAfter.Sub(time.Now())
+    BoldPrintf("Expires in: ")
+    fmt.Printf("%v days\n", math.Floor(timeDifference.Hours()/24))
+    fmt.Printf("Version: %v ; Serial: %s\n", cert.Version, cert.SerialNumber)
+    fmt.Printf("BasicConstraintsValid: %v ; IsCA: %v ; MaxPathLen: %v\n",
+                cert.BasicConstraintsValid, cert.IsCA, cert.MaxPathLen)
 }
 
 func save_cert(cert *x509.Certificate) {
-        block := pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}
-        pemdata := string(pem.EncodeToMemory(&block))
+    block := pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}
+    pemdata := string(pem.EncodeToMemory(&block))
 
-        err := ioutil.WriteFile(cert.Subject.CommonName + ".pem", []byte(pemdata), 0644)
-	    if err != nil { panic(err) }
+    err := ioutil.WriteFile(cert.Subject.CommonName + ".pem", []byte(pemdata), 0644)
+    if err != nil { panic(err) }
 }
 
 func usage() {
@@ -59,44 +59,44 @@ func usage() {
 
 func main() {
 
-	var chain = flag.Bool("chain", false, "Print details about all the certificates on the chain")
-	var save = flag.Bool("save", false, "Save the PEM to the local disk in the format of #{CN}.pem")
-	flag.Parse()
+    var chain = flag.Bool("chain", false, "Print details about all the certificates on the chain")
+    var save = flag.Bool("save", false, "Save the PEM to the local disk in the format of #{CN}.pem")
+    flag.Parse()
 
     args := flag.Args()
     if len(args) < 1 {
-    	usage()
+        usage()
     }
 
-	config := tls.Config{InsecureSkipVerify: true}
-	conn, err := net.DialTimeout("tcp", args[0], 2*time.Second)
+    config := tls.Config{InsecureSkipVerify: true}
+    conn, err := net.DialTimeout("tcp", args[0], 2*time.Second)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	client := tls.Client(conn, &config)
-	err = client.Handshake()
+    client := tls.Client(conn, &config)
+    err = client.Handshake()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	state := client.ConnectionState()
-	
-	certificates := []*x509.Certificate{}
-	if *chain {
-		certificates = state.PeerCertificates
-	} else {
-		certificates = []*x509.Certificate{state.PeerCertificates[0]}
-	}
+    state := client.ConnectionState()
 
-	for _, cert := range certificates {
-		print_cert(cert)
-		if *save {
-			save_cert(cert)
-		}
-		fmt.Println("--------------------")
-	}
-	client.Close()
+    certificates := []*x509.Certificate{}
+    if *chain {
+        certificates = state.PeerCertificates
+    } else {
+        certificates = []*x509.Certificate{state.PeerCertificates[0]}
+    }
+
+    for _, cert := range certificates {
+        print_cert(cert)
+        if *save {
+            save_cert(cert)
+        }
+        fmt.Println("--------------------")
+    }
+    client.Close()
 }
